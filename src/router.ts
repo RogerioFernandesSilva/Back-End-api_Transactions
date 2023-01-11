1810b4eec4b9a2a6070c8ad9ad5883bd21224313
Rogério Fernandes <106206470+RogerioFernandesSilva@users.noreply.github.com>
2023-01-11 20:35:15 -0300

    Commit Inicial

---
+++ src/router.ts
@@ -0,0 +1,65 @@
+import { Router, Request, Response } from "express";
+import {
+  readEmployees,
+  addClients,
+  editClient,
+  addTransaction,
+  readClientList,
+  thisClient,
+  removeClient,
+  readCLientTransactions,
+  customerSpecificTransaction,
+  editSpecificTransaction,
+  removeTransactions,
+} from "./functions";
+import checkRequests from "./middlewares";
+let router = Router();
+router.use(checkRequests);
+router.get("/", (req: Request, res: Response) => {
+  readEmployees(res);
+});
+// inicio a operação de clientes
+router.post("/clients", (req: Request, res: Response) => {
+  addClients(req, res);
+});
+
+router.get("/clients", (req: Request, res: Response) => {
+  readClientList(res);
+});
+
+router.get("/clients/:id", (req: Request, res: Response) => {
+  thisClient(req, res);
+});
+
+router.put("/clients/:id", (req: Request, res: Response) => {
+  editClient(req, res);
+});
+
+router.delete("/clients/:id", (req: Request, res: Response) => {
+  removeClient(req, res);
+});
+// fim
+
+// inicio das transações
+
+router.post("/clients/:userId/transactions", (req: Request, res: Response) => {
+  addTransaction(req, res);
+});
+
+router.get("/clients/:userId/transactions", (req: Request, res: Response) => {
+  readCLientTransactions(req, res);
+});
+
+router.get("/clients/:userId/transactions/:id", (req: Request, res: Response) => {
+  customerSpecificTransaction(req, res);
+});
+
+router.put("/clients/:userId/transactions/:id", (req: Request, res: Response) => {
+  editSpecificTransaction(req, res);
+});
+
+router.delete("/clients/:userId/transactions/:id", (req: Request, res: Response) => {
+  removeTransactions(req, res);
+});
+//fim
+export { router };
